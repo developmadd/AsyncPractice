a781b0a53fb160a899c738524cca00326a803644
Miguel Ochoa <miguel.adrecsa@gmail.com>
2020-01-28 19:21:49 -0600

    handler

--- app/src/main/java/com/example/asyncpractice/MainActivity.java
+++ app/src/main/java/com/example/asyncpractice/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.asyncpractice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -18,10 +21,12 @@ public class MainActivity extends AppCompatActivity {
     ProgressBar progressBarThread;
     ProgressBar progressBarAsyncTask;
     ProgressBar progressBarHandler;
+    ProgressBar progressBarHandlerClass;
     Button buttonNotSync;
     Button buttonThread;
     Button buttonAsyncTask;
     Button buttonHandler;
+    Button buttonHandlerClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +37,13 @@ public class MainActivity extends AppCompatActivity {
         buttonThread = findViewById(R.id.BTN_Thread);
         buttonAsyncTask = findViewById(R.id.BTN_AsyncTask);
         buttonHandler = findViewById(R.id.BTN_Handler);
+        buttonHandlerClass = findViewById(R.id.BTN_Handler_Class);
 
         progressBarNotSync = findViewById(R.id.PB_Not_Sync);
         progressBarThread = findViewById(R.id.PB_Thread);
         progressBarAsyncTask = findViewById(R.id.PB_AsyncTask);
         progressBarHandler = findViewById(R.id.PB_Handler);
-
+        progressBarHandlerClass = findViewById(R.id.PB_Handler_Class);
 
 
         buttonNotSync.setOnClickListener(new View.OnClickListener() {
@@ -131,16 +137,62 @@ public class MainActivity extends AppCompatActivity {
         buttonHandler.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                progressBarHandler.setMax(10);
-                new Handler().post(new Runnable() {
+
+                final Handler handler = new Handler();
+                new Thread(new Runnable() {
                     @Override
                     public void run() {
+
                         for( int i = 1 ; i <= 10 ; i++){
                             task();
-                            progressBarHandler.setProgress(i);
+                            final int finalI = i;
+                            handler.post(new Runnable(){
+                                @Override
+                                public void run() {
+                                    progressBarHandler.setProgress(finalI);
+                                }
+                            });
+
                         }
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), "Listo Handler", Toast.LENGTH_LONG).show();
+                            }
+                        });
+
                     }
-                });
+                }).start();
+
+            }
+        });
+
+
+        buttonHandlerClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final Handler handler = new Handler(){
+                    @Override
+                    public void handleMessage(@NonNull Message msg) {
+                        //if(msg)
+                        progressBarHandler.setProgress(msg.getData().getInt("value"));
+                        // else
+                        //oast.makeText(getApplicationContext(), "Listo Handler", Toast.LENGTH_LONG).show();
+                    }
+                };
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        for( int i = 1 ; i <= 10 ; i++){
+                            task();
+                            //handler.sendMessage(new Intent)
+                        }
+
+
+                    }
+                }).start();
             }
         });
 
